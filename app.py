33953a8e8ba4069ac2210ccebee14d98115a3256
panesarsupreet7 <panesarsupreet7@gmail.com>
2026-09-06 15:38:31 +0530

    Add initial Python application

---
+++ app.py
@@ -0,0 +1,16 @@
+def calculate_restaurant_bill(meal_cost):
+    
+    service_charge = 10 / 100 * meal_cost
+    Amount_service = meal_cost + service_charge
+    Tax = 5 / 100 * Amount_service
+    Tip = 5 / 100 * Amount_service
+    Total_bill = Amount_service + Tax + Tip
+    print(f'Meal Cost {meal_cost}')
+    print(f'Service charge (10%): {service_charge}')
+    print(f'Amount after service: {Amount_service}')
+    print(f'Tax (5%): {Tax}')
+    print(f'Tip (5%) {Tip}')
+
+       
+meal_cost = float(input(f"Enter the meal cost:"))
+calculate_restaurant_bill(meal_cost)
